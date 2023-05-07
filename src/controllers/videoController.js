8a0326ff45d7cc935f1511a594e09b1378790e40
nikejune <shoessniper@naver.com>
2023-05-07 09:56:10 +0900

    First Commit

---
+++ src/controllers/videoController.js
@@ -0,0 +1,65 @@
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
+
+export const trending = (req, res) => {
+  return res.render("home", { pageTitle: "Home", videos });
+};
+export const search = (req, res) => res.send("Search");
+
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", { pageTitle: `Editing ${video.title}`, video });
+};
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
+};
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+};
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload video" });
+};
+export const postUpload = (req, res) => {
+  const { title } = req.body;
+  const video = {
+    title,
+    rating: 0,
+    comments: 0,
+    createdAt: "Just now",
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(video);
+  return res.redirect(`/`);
+};
+export const deleteVideo = (req, res) => res.send("deleteVideo");
