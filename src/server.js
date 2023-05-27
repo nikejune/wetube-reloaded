125dd4e8b65c90a53267c62603cc5ba895402072
nikejune <shoessniper@naver.com>
2023-05-27 17:07:38 +0900

    session maxage is deleted

--- src/server.js
+++ src/server.js
@@ -21,11 +21,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
     secret: process.env.COOKIE_SECRET,
-    resave: true,
+    resave: false,
     saveUninitialized: false,
-    cookie: {
-      maxAge: 20000,
-    },
+    //cookie: {
+    //  maxAge: 20000,
+    //},
     store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
